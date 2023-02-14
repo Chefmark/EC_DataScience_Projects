ecb2aa2d2e014ed0fe4ab1848992f32438ac42cc
Markus Ekman <91083800+Chefmark@users.noreply.github.com>
2023-02-14 09:33:11 +0100

    Delete Python_Course/final_project directory

--- Python_Course/final_project/model_member.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-#class used to create members
-class Member(BaseModel):
-    id: int
-    first_name: str
-    last_name: str
-    phone_number: str
-    email: str
--- Python_Course/final_project/statistics_functions.py
+++
@@ -1,4 +0,0 @@
-# This file contains the functions used in store.py Statistics case
-
-def get_statistics():
-    pass
